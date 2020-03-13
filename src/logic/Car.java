646e9801a89fc54343983e35e4f8297c406f45fb
Soeren2208 <s.schwerk@schule.bremen.de>
2020-03-13 17:55:14 +0100

    beschleunigen

--- src/logic/Car.java
+++ src/logic/Car.java
@@ -24,4 +24,8 @@ public class Car {
     public void setActualSpeed(int actualSpeed) {
         this.actualSpeed = actualSpeed;
     }
+
+    public void beschleunigen(){
+        this.actualSpeed+=10;
+    }
 }
