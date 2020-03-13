e76eeff19cc5ee271c3bfd6c2f22eadce9cf54d2
Soeren2208 <s.schwerk@schule.bremen.de>
2020-03-13 17:01:31 +0100

    Klasse Car hinzugefuegt

---
+++ src/logic/Car.java
@@ -0,0 +1,11 @@
+package logic;
+
+public class Car {
+    private double mileAge;
+    private int actualSpeed;
+
+    public Car(double mileAge, int actualSpeed){
+        this.mileAge=mileAge;
+        this.actualSpeed = actualSpeed;
+    }
+}
