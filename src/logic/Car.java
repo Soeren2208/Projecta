f670d979bfabf5ed0c7078d6d9f8ac46aa342828
Soeren2208 <s.schwerk@schule.bremen.de>
2020-03-13 17:42:11 +0100

    Getter und Setter

--- src/logic/Car.java
+++ src/logic/Car.java
@@ -8,4 +8,20 @@ public class Car {
         this.mileAge=mileAge;
         this.actualSpeed = actualSpeed;
     }
+
+    public double getMileAge() {
+        return mileAge;
+    }
+
+    public int getActualSpeed() {
+        return actualSpeed;
+    }
+
+    public void setMileAge(double mileAge) {
+        this.mileAge = mileAge;
+    }
+
+    public void setActualSpeed(int actualSpeed) {
+        this.actualSpeed = actualSpeed;
+    }
 }
